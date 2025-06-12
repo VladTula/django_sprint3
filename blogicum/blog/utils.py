48c9286c1a3a560569225a79a4aff6ff7808bae1
Vlad <dimon-eldorado@yandex.ru>
2025-06-12 21:18:45 +0300

    comit

--- blogicum/blog/utils.py
+++
@@ -1,7 +0,0 @@
-from django.utils import timezone
-from .models import Post
-
-
-def post_filter():
-    return Post.objects.filter(pub_date__lt=timezone.now(), is_published=True,
-                               category__is_published=True)
